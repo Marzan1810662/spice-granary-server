63d862dced35dc12a9392d6a27cf7b4d0132e159
Marzan1810662 <hassanmarzan17@gmail.com>
2022-04-30 13:37:41 +0600

    MongoDB connection and middlewares added

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
 const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kiy1o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const itemsCollection = client.db('spiceGranary').collection('items')
+
+        app.get('/items', async (req, res) => {
+            res.send('connected to mongo db')
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
+
 
 
 //root API
