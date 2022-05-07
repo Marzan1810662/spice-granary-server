d3d7a1bd82c057a5a1988686cd2ef7617f496052
Marzan1810662 <hassanmarzan17@gmail.com>
2022-05-08 00:21:24 +0600

    get api to get a single item

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion ,ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const port = process.env.PORT || 5000;
@@ -21,7 +21,7 @@ async function run() {
         await client.connect();
         const itemsCollection = client.db('spiceGranary').collection('items')
 
-        //Get -all items
+        //GET -all items
         app.get('/items', async (req, res) => {
             const query = {};
             const cursor = itemsCollection.find(query);
@@ -29,6 +29,19 @@ async function run() {
             const result = await cursor.toArray();
 
             res.send(result);
+        })
+
+        //GET - a specific item
+        app.get('/item/:id', async(req, res) => {
+            const id = req.params.id;
+
+            const query = { _id: ObjectId(id) }
+            console.log(query);
+
+            const item = await itemsCollection.findOne(query);
+
+            res.send(item);
+
         })
 
         //POST -insert one item
